19ffb3052f40e4ef0b48effc2892e7c87713474b
Haseeb Ahmed <haseeb.ahmed.cs@gmail.com>
2019-03-11 22:48:43 +0500

    App.js Push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter , Switch , Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import Signin from './components/auth/Signin'
 import Signup from './components/auth/Signup'
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Signin />
-        <Signup />
-
+        <Switch>
+          <Route exact path='/signin' component={Signin} />
+          <Route exact path='/signup' component={Signup} />
+          </Switch>
       </div>
+      </BrowserRouter>
+      
     );
   }
 }
